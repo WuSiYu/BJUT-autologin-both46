5f90c251557b2e9ead4c5b0c70193e5cfb1c7048
WuSiYu <wu.siyu@hotmail.com>
2018-11-08 15:31:08 +0800

    add get account info feature

--- gateway-login.py
+++ gateway-login.py
@@ -1,24 +1,22 @@
 #!/usr/bin/env python3
 
-from urllib import request
-import urllib
-import json, sys, time, re
-
-if len(sys.argv) != 3 and not (len(sys.argv) == 2 and sys.argv[1] == '--logout') :
-    print('BJUT network - Gateway Auto Login v0.1')
-    print('This script will auto login bjut\'s wired network both ipv4 and ipv6\n')
-    print('Usage: \t' + sys.argv[0] + ' <user> <password>')
-    print('\t' + sys.argv[0] + ' --logout')
-    sys.exit()
-
-LGN_LOGIN_URL = 'https://lgn.bjut.edu.cn'
+# === Settings BEGIN ===
+# Change it according to your school's network environment
+LGN_LOGIN_URL = 'https://lgn.bjut.edu.cn/'
 LGN_LOGOUT_URL = 'https://lgn.bjut.edu.cn/F.htm'
 LGN6_JUMPING_URL = 'https://lgn6.bjut.edu.cn/V6?https://lgn.bjut.edu.cn'
 
-# logout
-if len(sys.argv) == 2 and sys.argv[1] == '--logout':
+SIMULATED_UA = 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
+# === Settings END
+
+from urllib import request
+import urllib, sys, re
+
+
+# Logout current account
+def gateway_logout():
     req = request.Request(LGN_LOGOUT_URL)
-    req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0')
+    req.add_header('User-Agent', SIMULATED_UA)
     f = request.urlopen(req)
     data = f.read().decode('gb2312')
     if data.find('Msg=14') != -1:
@@ -26,60 +24,112 @@ if len(sys.argv) == 2 and sys.argv[1] == '--logout':
     else:
         print('Logout failed, have you already logged out?')
 
-    sys.exit()
 
-# Get lgn login page's content
-req = request.Request(LGN_LOGIN_URL)
-req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0')
-f = request.urlopen(req)
-data = f.read().decode('gb2312')
+# Get account status
+def gateway_account_status():
+    req = request.Request(LGN_LOGIN_URL)
+    req.add_header('User-Agent', SIMULATED_UA)
+    f = request.urlopen(req)
+    data = f.read().decode('gb2312')
+    # print(data)
+    result = re.match(r'.*time=\'(.+?)\s*\';', data, re.DOTALL)
+    if not result:
+        print('Error: re.match get account info failed, abort')
+        sys.exit(1)
+    used_time = int(result.groups()[0])
+    used_traffic = int(re.match(r'.*flow=\'(.+?)\s*\';', data, re.DOTALL).groups()[0]) / 1024
+    account_ballance = int(re.match(r'.*fee=\'(.+?)\s*\';', data, re.DOTALL).groups()[0]) / 10000
 
-# Get ipv4_server_ip from login page
-result = re.match(r'.*v4serip=\'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\'', data, re.DOTALL)
-if not result:
-    print('Error: re.match get ipv4_server_ip failed, have you already logged in?')
-    print('Please use the following command to logout:')
-    print('\t', sys.argv[0], '--logout')
-    sys.exit(1)
-
-ipv4_server_ip = result.groups()[0]
-print('ipv4_server_ip =', ipv4_server_ip)
-
-
-# POST data to lgn6 jumping page
-req = request.Request(LGN6_JUMPING_URL)
-req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0')
-post_data = {'DDDDD': sys.argv[1], 'upass': sys.argv[2], 'v46s': '0', 'v6ip': '', 'f4serip': ipv4_server_ip, '0MKKey': ''}
-post_data = urllib.parse.urlencode(post_data).encode()
-f = request.urlopen(req, post_data)
-data = f.read().decode('gb2312')
-
-# Get ipv6_server_ip from lgn6 jumping page
-result = re.match(r'.*name=\'v6ip\' value=\'(.+)\'', data, re.DOTALL)
-if not result:
-    print('Error: re.match get ipv6_server_ip failed, abort')
-    sys.exit(1)
-
-ipv6_server_ip = result.groups()[0]
-print('ipv6_server_ip =', ipv6_server_ip)
+    print("=== Account status =========================================")
+    print(" Connnected time:       %d mins (%d hours and %d mins)" % (used_time, used_time // 60, used_time % 60) )
+    print(" Traffic used:          %.2f GiB (%d GiB and %d MiB)" % (used_traffic / 1024, used_traffic // 1024, used_traffic % 1024) )
+    print(" Account ballance:      %d Yuan" % account_ballance)
+    print("=============================================================")
 
 
-# POST data to final lgn login page
-req = request.Request(LGN_LOGIN_URL)
-req.add_header('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0')
-post_data = {'DDDDD': sys.argv[1], 'upass': sys.argv[2], '0MKKey': 'Login', 'v6ip': ipv6_server_ip}
-post_data = urllib.parse.urlencode(post_data).encode()
-f = request.urlopen(req, post_data)
-data = f.read().decode('gb2312')
+# Login the bjut gateway
+def gateway_login(username, password):
+    # Get lgn login page's content
+    req = request.Request(LGN_LOGIN_URL)
+    req.add_header('User-Agent', SIMULATED_UA)
+    f = request.urlopen(req)
+    data = f.read().decode('gb2312')
 
-if data.find('successfully logged') != -1:
-    print('Login successed')
-elif data.find('Msg=01') != -1:
-    print('Error: wrong password')
-    sys.exit(1)
-else:
-    print('--- final lgn login page content dump - START')
-    print(data)
-    print('--- final lgn login page content dump - END')
-    print('\nError: unknown error, page content dumped')
-    sys.exit(1)
+    # Get ipv4_server_ip from login page
+    result = re.match(r'.*v4serip=\'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\'', data, re.DOTALL)
+    if not result:
+        if data.find('DispTFM') != -1:
+            print('Error: re.match get ipv4_server_ip failed, have you already logged in?\n'
+                  'Please use the following command to logout if you want:\n'
+                  '\t', sys.argv[0], '--logout\n')
+            print('Tring to get current logged in account status ...')
+            gateway_account_status()
+        else:
+            print('Error: re.match get ipv4_server_ip failed')
+        return -1
+
+    ipv4_server_ip = result.groups()[0]
+    print('ipv4_server_ip =', ipv4_server_ip)
+
+    # POST data to lgn6 jumping page
+    req = request.Request(LGN6_JUMPING_URL)
+    req.add_header('User-Agent', SIMULATED_UA)
+    post_data = {'DDDDD': username, 'upass': password, 'v46s': '0', 'v6ip': '', 'f4serip': ipv4_server_ip, '0MKKey': ''}
+    post_data = urllib.parse.urlencode(post_data).encode()
+    f = request.urlopen(req, post_data)
+    data = f.read().decode('gb2312')
+
+    # Get ipv6_server_ip from lgn6 jumping page
+    result = re.match(r'.*name=\'v6ip\' value=\'(.+?)\'', data, re.DOTALL)
+    if not result:
+        print('Error: re.match get ipv6_server_ip failed, abort')
+        sys.exit(1)
+
+    ipv6_server_ip = result.groups()[0]
+    print('ipv6_server_ip =', ipv6_server_ip)
+
+    # POST data to final lgn login page
+    req = request.Request(LGN_LOGIN_URL)
+    req.add_header('User-Agent', SIMULATED_UA)
+    post_data = {'DDDDD': username, 'upass': password, '0MKKey': 'Login', 'v6ip': ipv6_server_ip}
+    post_data = urllib.parse.urlencode(post_data).encode()
+    f = request.urlopen(req, post_data)
+    data = f.read().decode('gb2312')
+
+    if data.find('successfully logged') != -1:
+        print('Login successed, Welcome to the Internet!\n')
+        gateway_account_status()
+    elif data.find('Msg=01') != -1:
+        print('Error: wrong password')
+        return 1
+    elif data.find('Msg=04') != -1:
+        print('Error: server says: Insufficient account balance')
+        return 4
+    else:
+        print('--- final LGN login page content dump - START')
+        print(data)
+        print('--- final LGN login page content dump - END')
+        print('\nError: unknown error, page content dumped')
+        return -2
+
+
+def main():
+
+    if len(sys.argv) == 3:
+        gateway_login(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 2 and sys.argv[1] in ('-l', '--logout'):
+        gateway_logout()
+    elif len(sys.argv) == 2 and sys.argv[1] in ('-s', '--status'):
+        gateway_account_status()
+    else:
+        print('BJUT network - Gateway Auto Login v0.2\n'
+              'This script can log in to BJUT\'s wired network both ipv4 and ipv6\n'
+              'It may also works in other school\'s which using the same system\n\n'
+              'Usage: \t' + sys.argv[0] + ' <user> <password>\n'
+              '\t' + sys.argv[0] + ' -l, --logout\n'
+              '\t' + sys.argv[0] + ' -s, --status\n')
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
--- readme.md
+++ readme.md
@@ -1,16 +1,19 @@
 # BJUT Network ipv4&ipv6 Autologin
 北工大校园网有线自动登录Python3脚本，`IPV4与IPV6统一认证`模式。
 
+![screenshot](https://i.imgur.com/4FfrqPt.png)
+
 ## 简介
 北工大的校园网登录非常奇葩，如果你仅需要单一的ipv4登录或者ipv6登录，简单的使用`curl`发送一个POST请求就可以了，但这样无法同时使用ipv4和ipv6。若有此需求，只能通过`IPV4与IPV6统一认证`模式。在这个模式中，会先向跳转网址`https://lgn6.bjut.edu.cn/V6?https://lgn.bjut.edu.cn`发送POST请求，再向`https://lgn.bjut.edu.cn`发送第二个POST请求，跳转回来。过程中必须分别包含在页面中给出的v4和v6两个地址，才可正常的登录使用。
 
 所以本Python脚本通过正则表达式获取两个页面中的地址，完全模拟使用浏览器`IPV4与IPV6统一认证`登录时的各步骤，以达到可以同时使用ipv4和ipv6的效果。
 
-此脚本在本部宿舍有线网络上测试通过，暂不支持无线网络使用，通州校区暂未测试。
+此脚本在本部宿舍有线网络上测试通过，暂不支持无线网络使用。
 
 ## 使用
 ```
-Usage:  gateway-login.py <user> <password>     #登录
-        gateway-login.py --logout              #注销
+Usage:  gateway-login.py <user> <password>     #登录网关
+        gateway-login.py --status              #查看账号信息
+        gateway-login.py --logout              #注销账号
 ```
 如果报错无法登陆，请先尝试注销一遍。
